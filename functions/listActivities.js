66d226711854c03a1990ca643dc0872c0b839c18
camilon0 <kmilo8433@gmail.com>
2024-12-16 17:50:10 -0500

    CORS Changes in ListActivities

--- functions/listActivities.js
+++ functions/listActivities.js
@@ -1,15 +1,33 @@
-const AWS = require('aws-sdk');
+const AWS = require("aws-sdk");
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 const ACTIVITIES_TABLE = process.env.ACTIVITIES_TABLE;
 
-module.exports.handler = async () => {
+module.exports.handler = async (event) => {
+  // Manejar solicitudes preflight (OPTIONS) para CORS
+  if (event.httpMethod === "OPTIONS") {
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*", // Cambia "*" por un dominio específico si necesitas restringir el acceso, por ejemplo "http://localhost:3000".
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type",
+      },
+      body: null,
+    };
+  }
+
   try {
     // Verificar si la variable de entorno ACTIVITIES_TABLE está configurada
     if (!ACTIVITIES_TABLE) {
       return {
         statusCode: 500,
-        body: JSON.stringify({ message: "La tabla de actividades no está configurada correctamente." }),
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+        },
+        body: JSON.stringify({
+          message: "La tabla de actividades no está configurada correctamente.",
+        }),
       };
     }
 
@@ -25,6 +43,9 @@ module.exports.handler = async () => {
     if (!result.Items || result.Items.length === 0) {
       return {
         statusCode: 404,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+        },
         body: JSON.stringify({ message: "No se encontraron actividades." }),
       };
     }
@@ -32,12 +53,18 @@ module.exports.handler = async () => {
     // Devolver la lista de actividades
     return {
       statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
       body: JSON.stringify(result.Items),
     };
   } catch (error) {
     console.error("Error listando actividades", error);
     return {
       statusCode: 500,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
       body: JSON.stringify({ message: "Error interno del servidor." }),
     };
   }
